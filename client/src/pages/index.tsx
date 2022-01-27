41e64754769c80a171bcf53189ae85b7a7043128
idris agboola <agboolaisholaidreez@gmail.com>
2022-01-27 23:37:54 +0100

    update

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Layout from '../layout';
 import Welcome from '../components/welcome';
 import Send from '../components/send';
+import Service from '../components/service';
 
 const Home = () => {
   return (
     <Layout>
       <Welcome />
       <Send />
+      <Service />
     </Layout>
   );
 };
