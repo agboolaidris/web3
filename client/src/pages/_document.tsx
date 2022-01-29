37e3d71347cf7c1c065075b52bd54029cae147ec
idris agboola <agboolaisholaidreez@gmail.com>
2022-01-29 10:43:24 +0100

    update

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -15,6 +15,16 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          <link
+            rel="preconnect"
+            href="https://fonts.googleapis.com"
+            crossOrigin="//#region "
+          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Princess+Sofia&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
